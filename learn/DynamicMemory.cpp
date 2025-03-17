aa3126f5ff92012e913e0cc1895b371ba72411fe
arasruislam <arasru01@gmail.com>
2025-03-17 10:21:48 +0600

    dynamic memory

--- learn/DynamicMemory.cpp
+++ learn/DynamicMemory.cpp
@@ -2,9 +2,23 @@
 
 using namespace std;
 
+int* fun(){
+   int *a = new int[5];
+   for (int i = 0; i < 5;i ++){
+      cin >> a[i];
+   }
+      return a;
+}
+
 int main()
 {
-    
+   int *a = fun();
 
-    return 0;
+   for (int i = 0; i < 5; i++)
+   {
+      cout << a[i] << endl;
+   
+   }
+
+   return 0;
 }
